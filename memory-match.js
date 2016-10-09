d8e99742f0674e9275f0ba9eea246223776ccdfd
John (Jef) Fisher <Fishe545@gmail.com>
2016-10-09 01:43:48 -0700

    Memory Match v1.0 - John (Jef) Fisher

---
+++ memory-match.js
@@ -0,0 +1,85 @@
+/**
+ * Created by Katsurio on 10/8/16.
+ */
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = 9;
+var match_counter = 0;
+var matches= 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
+
+function display_stats() {
+    accuracy = matches / attempts + '%';
+    $('.games-played .value').text(games_played);
+    $('.attempts .value').text(attempts);
+    $('.accuracy .value').text();
+}
+
+function reset_stats() {
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    display_stats();
+    $('.victory-h1').remove();
+    $(first_card_clicked).removeClass('clicked');
+    $(second_card_clicked).removeClass('clicked');
+    first_card_clicked = null;
+    second_card_clicked = null;
+    $('.card').click(card_clicked).find('.back').show();
+
+}
+
+function card_clicked() {
+    if($(this).hasClass('clicked')) {
+        return;
+    }
+    $(this).find(".back").hide();
+    if (first_card_clicked === null) {
+        console.log("1st card = null");
+        first_card_clicked = $(this).addClass('clicked');
+        console.log(first_card_clicked);
+    } else {
+        console.log("else/1st card != null");
+        second_card_clicked = $(this).addClass('clicked');
+        console.log(second_card_clicked);
+        if($(first_card_clicked).find('.front img').attr('src') == $(second_card_clicked).find('.front img').attr('src')) {
+            console.log("we have a match");
+            match_counter++;
+            matches++;
+            ++attempts;
+            console.log('attempts/match: ', attempts);
+            first_card_clicked = null;
+            second_card_clicked = null;
+            if(match_counter === total_possible_matches) {
+                $('.main-content').append($('<h1>').addClass('victory-h1').text("Victory!"));
+            }
+            return false;
+        } else {
+            $('.card').off();
+            ++attempts;
+            console.log('attempts/not a match', attempts);
+            function two_cards_mismatch_timeout() {
+                $(first_card_clicked).removeClass('clicked').find(".back").show();
+                $(second_card_clicked).removeClass('clicked').find(".back").show();
+                first_card_clicked = null;
+                second_card_clicked = null;
+                $('.card').click(card_clicked);
+            }
+            setTimeout(two_cards_mismatch_timeout, 2000);
+        }
+    }
+    display_stats();
+}
+
+function apply_click_handlers(){
+    $('.card').click(card_clicked);
+    $('.reset').click(function() {
+        games_played++;
+        reset_stats();
+        display_stats();
+        $('.card').find('.back').show();
+    })
+}
+$(document).ready(apply_click_handlers);
